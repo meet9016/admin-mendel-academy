4bd6726d3c82f461f346378dc95962fe6fc340e8
meet <meet.shopno@gmail.com>
2025-12-12 11:09:17 +0530

    axios file change

--- src/utils/axiosInstance.tsx
+++ src/utils/axiosInstance.tsx
@@ -2,16 +2,16 @@ import axios from 'axios'
 
 // const baseURL = 'http://localhost:3699/api/v1/';
 // const baseURL = 'https://christeen-unmaidenlike-bloomingly.ngrok-free.dev/api/v1/';
-const baseURL = 'http://192.168.29.254:3699';
+// const baseURL = 'http://192.168.29.254:3699';
 // const baseURL = process.env.NEXT_PUBLIC_APP_URL;
 
 const apiAdminInstance = axios.create({
-  baseURL,
+  baseURL: process.env.NEXT_PUBLIC_APP_URL,
   // headers: {
   //   'Content-Type': 'multipart/form-data'
   // }
 })
-console.log("baseURL", baseURL);
+console.log("baseURL *****", apiAdminInstance);
 
 export const api = apiAdminInstance;
 
